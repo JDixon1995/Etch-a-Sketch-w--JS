7032a1d6ccd00919e42d33e97fa6d184a06e55af
John <jddhalogamer@gmail.com>
2021-07-09 19:42:35 -0500

    started code on sketch grid, and basic css.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Etch-A-Sketch
+
+//Grid Generation
+
+for(i = 0; i < 256; i++) {
+    var div = document.createElement("div");
+    div.style.width = "500px";
+    div.style.height = "500px";
+    div.style.background = "white";
+    div.style.border = "black solid 1px";
+}
+
+document.getElementById("gridDiv").appendChild(div);
