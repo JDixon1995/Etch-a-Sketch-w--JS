f99387799b54edd14f4f550813412ca0912c53f4
John <jddhalogamer@gmail.com>
2021-07-11 14:38:29 -0500

    added a reset button to clear the grid-marking.

--- script.js
+++ script.js
@@ -5,12 +5,19 @@
 const container = document.getElementById("gridDiv");
 
 function makeRows(rows, columns) {
+    const resetButton = document.getElementById("resetButton");
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-columns", columns);
     for(c = 0; c < (rows * columns); c++) {
         let cell = document.createElement("div");
         cell.innerText = c + 1;
         cell.className = "grid-cell";
+        cell.addEventListener("mouseover", function() {
+            cell.className = "grid-item-darkened";
+        })
+        resetButton.addEventListener("click", function() {
+            cell.className = "grid-item";
+        })
         cell.setAttribute('style','font-size:2rem, border:1px solid black');
         container.appendChild(cell).className = "grid-item";
     };
